1cc5f61f1ca9f44245fe1d80e9db4d99acaae6bc
Glitch (nostalgic-apparatus) <none>
2018-02-14 19:10:33 +0000

    👓📷 Checkpoint server.js:683828/229 config.js:683828/1

--- server.js
+++ server.js
@@ -16,22 +16,15 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+app.get("/imagasearch/:img",function(req, res, next){
+  let img = req.params.img;
+  let offset = req.params.offset;
+  console.log(req.params.img)
+  if(img){
+    res.json({img: img})
+  }
+  next();
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
