5c79fdb89fcca41e42cf210d64d017db608ba926
Glitch (nostalgic-apparatus) <none>
2018-02-14 19:21:25 +0000

    📮💘 Checkpoint server.js:683828/213 config.js:683828/88

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  
+ authorization: 'Client-ID 86ac1a737f1736a' 
 }
--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 // init project
 var express = require('express');
 var app = express();
+var config = require('./config');
+var http = require('http');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -16,12 +18,39 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+//https://api.imgur.com/3/gallery/search/?q=cats
+var options = {
+  host: 'api.imgur.com',
+  path: '/index.html'
+};
+
 app.get("/imagasearch/:img",function(req, res, next){
   let img = req.params.img;
   let offset = req.params.offset;
   console.log(req.params.img)
   if(img){
-    res.json({img: img})
+    //http.get();
+    //res.json({img: img})
+    options.path += '?q=' + img; 
+
+    http.get(options, function(resource) {
+      //res.json({resource: resource})
+      
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+      // Buffer the body entirely for processing as a whole.
+      var bodyChunks = [];
+      resource.on('data', function(chunk) {
+        // You can process streamed parts here...
+        bodyChunks.push(chunk);
+      }).on('end', function() {
+        var body = Buffer.concat(bodyChunks);
+        console.log('BODY: ' + body);
+        // ...and/or process the entire body here.
+        res.j
+      })
+    })
   }
   next();
 })
